caaa210ff375cf1237eaffff43919323f9e10006
siriabarrios88 <siriabarrios88@gmail.com>
2023-02-17 23:54:05 -0300

    cambio tamano de logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { RedessocialesComponent } from './components/redessociales/redessociales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoComponent
+    LogoComponent,
+    RedessocialesComponent
   ],
   imports: [
     BrowserModule,
